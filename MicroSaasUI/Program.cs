4bbdf35a1af4783cfc74d5433b5f1dd2be94a1f2
bennydanyael <bennydanyael@gmail.com>
2024-10-05 19:26:26 +0700

    Add project files.

---
+++ MicroSaasUI/Program.cs
@@ -0,0 +1,12 @@
+using MicroSaasUI;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddFluentUIComponents();
+builder.Services.AddDataGridEntityFrameworkAdapter();
+await builder.Build().RunAsync();
